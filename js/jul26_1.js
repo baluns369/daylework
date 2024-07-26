4a47465be83c39408b06dbac4a14e77e08129695
baluns369 <baluns2003@gmail.com>
2024-07-26 13:28:43 +0530

    done

---
+++ js/jul26_1.js
@@ -0,0 +1,80 @@
+// a=[1,2,3,4,5,6]
+// b=[...a]
+// b[0]=10
+// console.log(a)
+// console.log(b)
+
+
+// c={name:"alam"}
+
+// a={name:"balu",age:20,marck:68}
+// let {name,age,marck}=a
+// console.log(name,age,marck)
+
+
+
+
+// x=[1,2,3,4,5,6,7,8,9]
+// let [a,b,c,d,e,f,g,h]=x
+// console.log(a,b,c,d,e,g,h)
+
+
+
+// a={name:"balu",age:20,class:10,skill:{frend:"html,css",back:"python,java",db:"mongodb,sql"}}
+// let {name,age,skill:{frend,back,db}} =a
+// console.log(name,age,back)
+
+
+
+// x=[]
+
+
+
+
+
+
+
+
+
+// function sndMsg(msg)
+//     {
+//         console.log("send masseg")
+//         console.log(msg)
+//     }
+// function getMsg(callback)
+// {
+//     console.log("get massage")
+//     callback("hello wellcome")
+// }
+// getMsg(sndMsg)
+
+
+ob=[
+    {name:"joy",dep:"MERN",salary:"30000",exp:1},
+    {name:"rahul",dep:"MERN",salary:"45000",exp:2},
+    {name:"sree",dep:"ui ux",salary:"20000",exp:1},
+    {name:"anil",dep:"python",salary:"55000",exp:3},
+    {name:"joy",dep:"ui ux",salary:"22000",exp:1},
+]
+
+
+
+// for (i in ob)
+// {
+//     console.log(ob[i].name)
+// }
+
+
+// ob.map((item)=>{
+//     console.log(item.name)
+// })
+
+
+
+// let x=ob.find((item)=>{
+//     return item.name==="joy"
+// })
+// console.log(x)
+
+
+
